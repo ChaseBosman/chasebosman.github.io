2d5f334f05a8a1c1c894cc70a0e0060420078425
ChaseBosman <54570221+ChaseBosman@users.noreply.github.com>
2020-09-02 21:58:39 -0700

    Added extras page, created better looking menu
    Plan to implement displaying of specific projects, coursework, and resume to extras page

---
+++ main.js
@@ -0,0 +1,19 @@
+function toggleNav() 
+{
+    var nav_dimensions = document.getElementById("mySidenav").offsetWidth;
+    if(nav_dimensions > 0)
+    {
+      closeNav();
+    }
+    else
+    {
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("items").style.marginLeft = "250px";
+    }
+}
+  
+function closeNav() 
+{
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("items").style.marginLeft = "0";
+} 
