f2fb1d9f4f0009b33880df0c6a637808a285a46d
ChaseBosman <54570221+ChaseBosman@users.noreply.github.com>
2020-09-13 18:45:37 -0700

    Adding javascript popup screens for extras page

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const list = document.querySelector(".popup-section .location");
+
 function toggleNav() 
 {
     var nav_dimensions = document.getElementById("mySidenav").offsetWidth;
@@ -17,3 +19,17 @@ function closeNav()
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("items").style.marginLeft = "0";
 } 
+
+function create()
+{
+  const li = document.createElement("li");
+  li.classList.add("state");
+  const markup = `
+    <h2 class="Data" data-name="region">
+      <span>Ca</span>
+      <sup>Us</sup>
+    </h2>
+    `;
+  li.innerHTML = markup;
+  list.appendChild(li); 
+}
