c178e9a04f337264c695c9523dc87107b9471997
ChaseBosman <54570221+ChaseBosman@users.noreply.github.com>
2020-09-20 12:20:44 -0700

    Created page/pdf display for resume

--- main.js
+++ main.js
@@ -19,14 +19,3 @@ function closeNav()
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("items").style.marginLeft = "0";
 } 
-
-function create()
-{
-  var x = document.getElementById("formL");
-
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
